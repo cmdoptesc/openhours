c645b515598dda2550819fcf7fb6902d8474de33
cmdoptesc <cmdoptesc@users.noreply.github.com>
2013-09-07 14:57:36 -0700

    bugfix.. isOpen was not returning true for certain late night hours, now show should work for all cases

--- sosch/restaurant.js
+++ sosch/restaurant.js
@@ -12,23 +12,16 @@ Restaurant.prototype.isOpen = function(dateObj) {
   var time = helpers.getTime(dateObj);
   var day = helpers.getDay(dateObj);
 
-  var cutoff = helpers.cutoff;
-
   if(typeof this.schedule[day] === 'undefined') { return false; }
   var open = this.schedule[day].open;
   var close = this.schedule[day].close;
 
-    // complicated checks for after midnight..
-  if(time < cutoff) {
-    if(close <= cutoff && time < close) {
-      if(open < cutoff && open < time) {
-        return true;
-      } else if(open >= cutoff && open <= 24) {
-        return true;
-      }
-    }
-  } else if(open < time && time < close) {
+  if(open < close && open <= time && time < close) {
     return true;
+  } else if(open > close) {     // if it rolls over to the next day (e.g. 1800 - 0200)
+    if( (open < time && time <= 24) || (0 <= time && time < close) ) {
+      return true;
+    }
   }
   return false;
 };
