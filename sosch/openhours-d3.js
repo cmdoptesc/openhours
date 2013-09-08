305731f61e7d74edf66e1b67ad20bad26fe7d9e0
cmdoptesc <cmdoptesc@users.noreply.github.com>
2013-09-07 22:55:10 -0700

    very basic d3 visualisation

---
+++ sosch/openhours-d3.js
@@ -0,0 +1,65 @@
+
+$(function() {
+  d3.select("#ChartArea").append('svg:svg')
+    .style('border', '1px solid rgba(153,153,153, 0.5)').attr("id",'openSpots');
+
+  find_open_restaurants('rest_hours.csv', new Date(), function(openSpots) {
+    render(openSpots);
+  });
+});
+
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var render = function(dataset) {
+  console.log('called');
+  var vis = d3.select("#openSpots");
+
+    // where the bar chart starts
+  var hr_offset = 6;
+
+  var max = d3.max(dataset, function(d) {
+    return d.close;
+  });
+
+  var xScale = d3.scale.linear().domain([hr_offset, max+1]).range([0, width]),
+      xValue = function(d) { return xScale(d.close - (d.open-hr_offset)); },
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(function(d){
+        var hr = d%24;
+
+        if(hr < 12) {
+          hr = (hr === 12) ? '12 am' : hr + ' am';
+        } else {
+          hr = hr + ' pm';
+        }
+        return hr;
+      });
+
+  // var yValue = function(d) { return d.name; },
+  //     yScale = d3.scale.ordinal().rangeRoundBands([0, width], .1), // value -> display
+  //     yMap = function(d) { return yScale(yValue(d)); }, // data -> display
+  //     yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+  vis.append("g")
+      .attr("class", "x_axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  var gBar = vis.selectAll("g.bar-group");
+  gBar = gBar.data(dataset);
+
+  var group = gBar.enter().append("svg:g").attr("class", 'bar-group');
+
+  group.append('rect')
+    .attr("x", function(d){
+      return xScale(d.open);
+    })
+    .attr("y", function(d, i){
+      return i*10;
+    })
+    .attr("width", xValue)
+    .attr("height", 4);
+
+};
